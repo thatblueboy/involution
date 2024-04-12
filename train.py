fc6380ec00839a6f7e5c227562d68f88d7544bed
AtharvMane <atharvmeenakshi@gmail.com>
2024-04-13 00:06:17 +0530

    added logger

--- train.py
+++ train.py
@@ -11,18 +11,20 @@ pl.seed_everything(42, workers=True)
 if __name__=="__main__":
 
     configs={
+	'experiment_name': "New_expmt",
         'dataset': CIFAR100(root=".", train=True, download=True),
         'ReDSnet_type':50,
         'batch_size': 8,
         'num_classes': 200,
         'optimizer': Adam,
         'learning_rate': 0.005,
-        'num_workers':8
+        'num_workers':8,
+	'max_epochs'=100
     }
     checkpoint_callback = ModelCheckpoint(monitor="test/epoch_accuracy", save_last=True, save_on_train_epoch_end=True)
-    
+    logger = WandbLogger(name=configs['experiments_name'])
     data_module = ClassificationDataModule(configs['dataset'], batch_size=configs['batch_size'], num_workers=configs['num_workers'])
     model = RedNetClassifier(configs['ReDSnet_type'] ,configs['num_classes'], configs['optimizer'], configs['learning_rate'])
-    trainer = pl.Trainer(max_epochs=100, check_val_every_n_epoch=2,callbacks=[checkpoint_callback])
+    trainer = pl.Trainer(max_epochs=configs['max_epochs'], check_val_every_n_epoch=2,callbacks=[checkpoint_callback], logger=logger)
     trainer.fit(model, datamodule=data_module)
     
